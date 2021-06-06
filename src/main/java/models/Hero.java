5a378dcc43abb94511be1ffa363b04802d277ca6
OderoOluoch <oderoluoch@gmail.com>
2021-06-06 21:33:12 +0300

    Implemented Hero class constructor method

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,15 @@
 package models;
 
 public class Hero {
+    private String name;
+    private int age;
+    private String weakness;
+    private String powers;
+
+    public Hero(String name, int age,String weakness, String powers){
+        this.name = name;
+        this.age = age;
+        this.weakness = weakness;
+        this.powers = powers;
+    }
 }
