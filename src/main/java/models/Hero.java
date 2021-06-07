1bfedb7a2aed3fe930345c038a09e508b6b98a8c
OderoOluoch <oderoluoch@gmail.com>
2021-06-07 13:46:18 +0300

    Hero class created and tested succesfully

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -10,13 +10,60 @@ public class Hero {
     private static ArrayList<Hero> instances = new ArrayList<>();
     private int id;
 
-    public Hero(String name, int age,String weakness, String powers, int id){
+    public Hero(String name, int age,String weakness, String powers){
         this.name = name;
         this.age = age;
         this.weakness = weakness;
         this.powers = powers;
-        this.id = id;
+        this.id = instances.size();
         instances.add(this);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+    public String getPowers() {
+        return powers;
+    }
+
+    public void setPowers(String powers) {
+        this.powers = powers;
+    }
+
+    public static ArrayList<Hero> getInstances() {
+        return instances;
+    }
+
+    public static void setInstances(ArrayList<Hero> instances) {
+        Hero.instances = instances;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,15 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeroTest {
+
+    @Test
+    public void NewHeroObjectGetsCorrectlyCreated_true(){
+        Hero hero = new Hero("Odero",24,"Light","Eyes");
+        assertEquals(true,hero instanceof Hero);
+    }
 
 }
