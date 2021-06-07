84c1813d7a1d1b1d20b3b8f3c04e650bf2fa0ca7
OderoOluoch <oderoluoch@gmail.com>
2021-06-07 10:36:30 +0300

    Updated the Hero class

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,15 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Hero {
     private String name;
     private int age;
     private String weakness;
     private String powers;
+    private static ArrayList<Hero> instances = new ArrayList<>();
+    private int id;
 
-    public Hero(String name, int age,String weakness, String powers){
+    public Hero(String name, int age,String weakness, String powers, int id){
         this.name = name;
         this.age = age;
         this.weakness = weakness;
         this.powers = powers;
+        this.id = id;
+        instances.add(this);
     }
 }
