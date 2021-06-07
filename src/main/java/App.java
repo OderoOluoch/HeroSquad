46436ddb7cd0b875c2c60821575e700691df9259
OderoOluoch <oderoluoch@gmail.com>
2021-06-07 23:55:17 +0300

    Implementing forms and getting user inouts

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,45 @@
+import models.Hero;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import static spark.Spark.*;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class App {
     public static void main(String[] args) {
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-//            ArrayList<Post> posts = Post.getAll();
-//            model.put("posts", posts);
-
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        //Url for hero list
+        get("/heroes", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> heroes = Hero.getAll();
+            model.put("heroes", heroes);
+            return new ModelAndView(model, "heroList.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show new post form
+        get("/heroes/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "heroForm.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/heroes/new", (req, res) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<>();
+
+            String name = req.queryParams("content");
+            int age = Integer.parseInt(req.queryParams("age"));
+
+            Hero newHero = new Hero(name,age,);
+            model.put("hero", newHero);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
