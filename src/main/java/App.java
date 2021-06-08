6ad0269439f21e711becec5186b4e10066f57a6f
OderoOluoch <oderoluoch@gmail.com>
2021-06-08 12:49:27 +0300

    Templating the squads section

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -45,6 +45,51 @@ public class App {
             return new ModelAndView(model, "heroSuccess.hbs");
         }, new HandlebarsTemplateEngine());
 
+        //get: show an individual hero
+        get("/heroes/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfHeroToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            Hero foundHero = Hero.findById(idOfHeroToFind); //use it to find post
+            model.put("hero", foundHero); //add it to model for template to display
+            return new ModelAndView(model, "heroDetail.hbs"); //individual post page.
+        }, new HandlebarsTemplateEngine());
+
+
+        //get: show a form to update a post
+        get("/heroes/:id/update", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfHeroToEdit = Integer.parseInt(req.params("id"));
+            Hero editHero = Hero.findById(idOfHeroToEdit);
+            model.put("editHero", editHero);
+            return new ModelAndView(model, "heroForm.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //post: process a form to update a post
+        post("/heroes/:id/update", (req, res) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<>();
+            String newName = req.queryParams("name");
+            int idOfHeroToEdit = Integer.parseInt(req.params("id"));
+            Hero editHero = Hero.findById(idOfHeroToEdit);
+            editHero.updateName(newName);
+            return new ModelAndView(model, "heroSuccess.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: delete an individual post
+        get("/heroes/:id/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfHeroToDelete = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            Hero deleteHero = Hero.findById(idOfHeroToDelete); //use it to find post
+            deleteHero.deleteHero();
+            return new ModelAndView(model, "heroSuccess.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: delete all posts
+        get("/heroes/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            Hero.clearAllHeroes();
+            return new ModelAndView(model, "heroSuccess.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
 
         //Url for squad list
@@ -64,13 +109,61 @@ public class App {
         post("/squads/new", (req, res) -> { //URL to make new post on POST route
             Map<String, Object> model = new HashMap<>();
 
-            String name = req.queryParams("content");
-            int age = Integer.parseInt(req.queryParams("age"));
+            String name = req.queryParams("name");
+            int size = Integer.parseInt(req.queryParams("size"));
+            String cause = req.queryParams("cause");
 
-//           Squad newSquad = new Squad();
-//            model.put("hero", newSquad);
-            return new ModelAndView(model, "success.hbs");
+            Squad newSquad = new Squad(name,size,cause);
+            model.put("squad", newSquad);
+            return new ModelAndView(model, "squadSuccess.hbs");
         }, new HandlebarsTemplateEngine());
+
+        //get: show an individual squad
+        get("/squads/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfSquadToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            Hero foundSquare = Hero.findById(idOfSquadToFind); //use it to find post
+            model.put("squad", foundSquare); //add it to model for template to display
+            return new ModelAndView(model, "squadDetail.hbs"); //individual post page.
+        }, new HandlebarsTemplateEngine());
+
+
+        //get: show a form to update a post
+        get("/squads/:id/update", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfSquadToEdit = Integer.parseInt(req.params("id"));
+            Squad editSquad = Squad.findById(idOfSquadToEdit);
+            model.put("editSquad", editSquad);
+            return new ModelAndView(model, "squadForm.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //post: process a form to update a post
+        post("/squads/:id/update", (req, res) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<>();
+            String newName = req.queryParams("name");
+            int idOfSquadToEdit = Integer.parseInt(req.params("id"));
+            Squad editSquad = Squad.findById(idOfSquadToEdit);
+            editSquad.updateName(newName);
+            return new ModelAndView(model, "squadSuccess.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: delete an individual post
+        get("/squads/:id/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfSquadToDelete = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            Squad deleteSquad = Squad.findById(idOfSquadToDelete); //use it to find post
+            deleteSquad.deleteSquad();
+            return new ModelAndView(model, "squadSuccess.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: delete all squads
+        get("/squads/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            Squad.clearAllSquads();
+            return new ModelAndView(model, "squadSuccess.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
 
 
     }
--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -63,9 +63,6 @@ public class Hero {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public static ArrayList<Hero> getAll(){
         return instances;
