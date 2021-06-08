4c32acb2832e7daea36a7213f2973cf997847e67
OderoOluoch <oderoluoch@gmail.com>
2021-06-08 10:26:56 +0300

    Templates routing successfully

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import models.Hero;
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -36,10 +37,38 @@ public class App {
             String name = req.queryParams("content");
             int age = Integer.parseInt(req.queryParams("age"));
 
-            Hero newHero = new Hero(name,age,);
-            model.put("hero", newHero);
+//            Hero newHero = new Hero(name,age,);
+//            model.put("hero", newHero);
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
+
+
+
+        //Url for squad list
+        get("/squads", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Squad> squads = Squad.getAll();
+            model.put("squads", squads);
+            return new ModelAndView(model, "squadList.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show new squad form
+        get("/squads/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "squadForm.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/squads/new", (req, res) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<>();
+
+            String name = req.queryParams("content");
+            int age = Integer.parseInt(req.queryParams("age"));
+
+//           Squad newSquad = new Squad();
+//            model.put("hero", newSquad);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
     }
 }
